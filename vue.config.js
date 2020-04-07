fa97c77206581a0be4e5d536efb5b1c8dd894105
pan463859 <panan521@qq.com>
2020-04-07 19:14:14 +0800

    直接添加到具体的文件夹名字吧

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 module.exports = {
     // 基本路径
-    publicPath: './',
+    publicPath: "/blog/",
     outputDir: "dist",
-    assetsDir:"static",
-    indexPath:'index.html',
-    // 生产环境是否生成 sourceMap 文件
-    productionSourceMap: false,
-  
+    assetsDir: "static",
+    indexPath: 'index.html',
+
 }
