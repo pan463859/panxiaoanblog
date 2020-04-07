7b7d2d7379a71887cd19746aec5f4876913dbaea
pan463859 <panan521@qq.com>
2020-04-07 18:43:10 +0800

    修改vue.config.js的配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     // 基本路径
     publicPath: './',
+    outputDir: "dist",
+    assetsDir:"static",
+    indexPath:'index.html',
     // 生产环境是否生成 sourceMap 文件
     productionSourceMap: false,
   
