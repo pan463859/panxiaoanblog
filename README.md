5430c2f9f20541609e978ce9796e29bf6ec61547
pan463859 <panan521@qq.com>
2020-04-07 15:59:35 +0800

    博客初始化

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# deployment
+# panxiaoan
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/avator.jpg">
+    <HelloWorld/>
   </div>
 </template>
 
@@ -23,6 +23,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px
+}
+#app img{
+  display: inline-block;
+  width: 120px;
 }
 </style>
